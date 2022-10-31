a1d670edfb48a00d1be3ffe9bffc7da33bdd974c
LRittes <leandro_rittes@hotmail.com>
2022-10-31 09:34:51 -0300

    finish tarefa 1

--- tarefa1/lib/main.dart
+++ tarefa1/lib/main.dart
@@ -30,49 +30,180 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.black,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Spacer(flex: 5),
-          Container(
-            height: 50,
-            width: 50,
-            color: Colors.red,
+          SizedBox(
+            height: 85,
+            width: 90,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(221, 99, 46, 148),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(221, 99, 46, 148),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  width: 40,
+                  height: 85,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(221, 99, 46, 148),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(40),
+                      bottomLeft: Radius.circular(40),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           const Spacer(flex: 1),
-          Container(
-            height: 50,
-            width: 50,
-            color: Colors.blue,
+          SizedBox(
+            height: 110,
+            width: 200,
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text("Texto 1"),
-                  Text("Texto 2"),
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: const [
+                      Text(
+                        "Get your Money",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Under Control",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Column(
+                    children: const [
+                      Text(
+                        "Manage your expenses.",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        "Seamlessly.",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
                 ]),
           ),
           const Spacer(flex: 2),
-          Container(
-              height: 50,
-              width: 50,
-              color: Colors.yellow,
+          SizedBox(
+              height: 90,
+              width: 250,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
-                    height: 10,
+                    height: 40,
                     width: double.infinity,
-                    color: Colors.red,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(221, 99, 46, 148),
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                    ),
+                    child: const Text(
+                      "Sign Up with Email Id",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ),
                   Container(
-                    height: 10,
+                    height: 40,
                     width: double.infinity,
-                    color: Colors.red,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/images/logoG.jpg",
+                          width: 30,
+                          height: 30,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Text(
+                          "Sign Up with Google",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               )),
           const Spacer(flex: 1),
-          const Text("Already have an account?"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Already have an account ? ",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              Text(
+                "Sign in",
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
           const Spacer(flex: 1),
         ],
       ),
