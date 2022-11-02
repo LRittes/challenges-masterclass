7354c9bdb42dfe40617a9cf709172360eb4e9d54
LRittes <leandro_rittes@hotmail.com>
2022-11-01 22:42:58 -0300

    tarefa 2

--- tarefa2/lib/main.dart
+++ tarefa2/lib/main.dart
@@ -24,6 +24,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final text =
+      "If the sentence is the essential soul to express oneself in their own way, then the paragraph is the virtual body of it. ";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -44,31 +46,72 @@ class _MyHomePageState extends State<MyHomePage> {
             const Spacer(
               flex: 7,
             ),
-            Container(
-              height: 40,
-              width: 40,
-              color: Colors.amber,
+            SizedBox(
+              height: 60,
               child: Row(
-                children: const [
-                  Text("Tinder"),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/tinder_icon.png"),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "Tinder",
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
               ),
             ),
             const Spacer(
               flex: 4,
             ),
-            Container(
+            SizedBox(
               height: 40,
-              width: 40,
-              color: Colors.red,
+              width: 200,
+              child: Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 10,
+                  color: Colors.white70,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
             const Spacer(
               flex: 1,
             ),
             Container(
-              height: 40,
-              width: 40,
+              height: 100,
+              width: 200,
               color: Colors.blueGrey,
+              child: Column(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 45,
+                    color: Colors.green,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 20,
+                          height: 20,
+                          color: Colors.red,
+                        ),
+                        const Expanded(
+                          child: Text("text 1", textAlign: TextAlign.center),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
             const Spacer(
               flex: 1,
